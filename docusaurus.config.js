b7743927282b0fc83c5c6158437c74151003b713
andres meira <andresmeira@gmail.com>
2023-02-16 12:44:36 -0600

    1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -138,7 +138,7 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       colorMode: {
-        defaultMode: 'dark',
+        defaultMode: 'light',
         disableSwitch: false,
         respectPrefersColorScheme: false,
       },

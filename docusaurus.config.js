72a1b1dffbda59f960a4c52fbdd254cfe5618e2a
andres meira <andresmeira@gmail.com>
2023-02-16 11:55:56 -0600

    1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,10 +72,10 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Grillo',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Grillo Logo',
+          src: 'img/grillo-black-logo.svg',
         },
         items: [
           {

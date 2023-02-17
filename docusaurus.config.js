ae35245ed10358fa4608188dc983ad0f199dca75
andres meira <andresmeira@gmail.com>
2023-02-17 13:39:13 -0600

    links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,7 +89,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/grillo/grillo-docs',
             label: 'GitHub',
             position: 'right',
           },

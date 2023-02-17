68a41e648d0b5ef65734e59ff594db8f5b2540f9
andres meira <andresmeira@gmail.com>
2023-02-17 14:08:54 -0600

    add app login to menu

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,10 +92,13 @@ const config = {
             position: 'left',
             label: 'Docs',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/blog', 
+          label: 'Blog', 
+          position: 'left'
+          },
           {
-            href: 'https://github.com/grillo/grillo-docs',
-            label: 'GitHub',
+            to: 'https://app.grillo.io/',
+            label: 'Log in',
             position: 'right',
           },
         ],
